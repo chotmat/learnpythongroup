9106b712e672aa7a82a73fc9c5121c8d3bdd37fe
tan huynh <Tan Huynh>
2015-09-06 21:42:46 +0700

    Add all my work on LearnPythonTheHardWay

---
+++ LearnPythonTheHardWay/ex15.py
@@ -0,0 +1,14 @@
+# import from sys the module argv
+from sys import argv
+
+# unpack the argv to these variable
+script, filename = argv
+
+# open a file with the function open(filename) and assign it to txt
+txt = open(filename)
+
+print "Here's your file %r:" % filename
+# read content in the file txt and print it out
+print txt.read()
+
+txt.close()
